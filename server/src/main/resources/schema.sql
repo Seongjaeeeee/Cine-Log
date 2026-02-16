3520ed17027021f3ab67ba9be7eb04ef067947c2
Seongjaeeeee <162531436+Seongjaeeeee@users.noreply.github.com>
2026-02-16 05:03:05 +0000

    feat: 스키마 구조에 cascade 반영 및 테이블 컬럼 수정

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -23,7 +23,6 @@ CREATE TABLE movies (
     description        TEXT,
     release_date       DATE,
     rating             DOUBLE DEFAULT 0.0,
-    rating_policy_type VARCHAR(50),               -- 전략 패턴 구분값
     CONSTRAINT fk_movie_director FOREIGN KEY (director_id) REFERENCES directors(id)
 );
 
@@ -32,8 +31,8 @@ CREATE TABLE movie_actor (
     movie_id BIGINT NOT NULL,
     actor_id BIGINT NOT NULL,
     PRIMARY KEY (movie_id, actor_id),
-    CONSTRAINT fk_ma_movie FOREIGN KEY (movie_id) REFERENCES movies(id),
-    CONSTRAINT fk_ma_actor FOREIGN KEY (actor_id) REFERENCES actors(id)
+    CONSTRAINT fk_ma_movie FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
+    CONSTRAINT fk_ma_actor FOREIGN KEY (actor_id) REFERENCES actors(id) ON DELETE CASCADE
 );  
 
 
@@ -42,6 +41,7 @@ CREATE TABLE users (
     user_name  VARCHAR(100) NOT NULL UNIQUE,      -- String userName (중복 방지 UNIQUE 추가)
     password   VARCHAR(255) NOT NULL,             -- String password (암호화 대비 길게 설정)
     role       VARCHAR(20) NOT NULL,              -- Role Enum (USER, ADMIN 등 문자열 저장)
+    deleted    BOOLEAN DEFAULT FALSE,     -- 0: 활동중, 1: 탈퇴 (논리 삭제 플래그)
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 가입 시간 (추천)
 );
 
@@ -54,6 +54,6 @@ CREATE TABLE reviews (
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- (권장) 작성 시간
     
     -- 외래키 설정
-    CONSTRAINT fk_review_movie FOREIGN KEY (movie_id) REFERENCES movies(id),
+    CONSTRAINT fk_review_movie FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
     CONSTRAINT fk_review_user  FOREIGN KEY (user_id)  REFERENCES users(id)
 );
